2bbfa3d855114f391e86ab43bea12c7adf3fed36
Tamion <70228790+notTamion@users.noreply.github.com>
2023-09-30 10:38:45 +0200

    Message now no longer get sent to players in other worlds

--- src/main/java/de/tamion/listeners/ChatListener.java
+++ src/main/java/de/tamion/listeners/ChatListener.java
@@ -6,14 +6,16 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerChatEvent;
 
+import java.util.HashSet;
+
 public class ChatListener implements Listener {
     @EventHandler
     public void onChat(PlayerChatEvent e) {
         Player p = e.getPlayer();
         Double range = RangeChat.getPlugin().getConfig().getDouble("chat.range");
         if(range != 0.0) {
-            e.getPlayer().getWorld().getPlayers().forEach(target -> {
-                if(p.getLocation().distance(target.getLocation()) >= range) {
+            new HashSet<>(e.getRecipients()).forEach(target -> {
+                if(!p.getWorld().getPlayers().contains(target) || p.getLocation().distance(target.getLocation()) >= range) {
                     e.getRecipients().remove(target);
                 }
             });
