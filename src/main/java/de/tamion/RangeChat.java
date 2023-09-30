2d948a3bf2f0430d97aefdcf88c478d5e36c4bb0
Tamion <70228790+notTamion@users.noreply.github.com>
2023-09-30 10:38:45 +0200

    Added bStats

--- src/main/java/de/tamion/RangeChat.java
+++ src/main/java/de/tamion/RangeChat.java
@@ -5,6 +5,7 @@ import de.tamion.commands.RequireLineOfSight;
 import de.tamion.commands.SetChatRange;
 import de.tamion.listeners.ChatListener;
 import org.bukkit.Bukkit;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -15,12 +16,21 @@ public final class RangeChat extends JavaPlugin {
     public void onEnable() {
         plugin = this;
         PluginManager pluginManager = Bukkit.getPluginManager();
+        FileConfiguration config = getConfig();
 
         pluginManager.registerEvents(new ChatListener(), this);
 
         getCommand("setchatrange").setExecutor(new SetChatRange());
         getCommand("ignoreheight").setExecutor(new IgnoreHeight());
         getCommand("requirelineofsight").setExecutor(new RequireLineOfSight());
+
+        Metrics metrics = new Metrics(this, 18999);
+        metrics.addCustomChart(new Metrics.SimplePie("range", () -> {
+            if(config.contains("chat.range")) {
+                return config.getString("chat.range");
+            }
+            return "Disabled";
+        }));
     }
 
     public static RangeChat getPlugin() {
