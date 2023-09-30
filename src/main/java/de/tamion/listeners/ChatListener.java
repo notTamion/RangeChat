29519affab407d64339df42d6d6a299f98c709c6
Tamion <70228790+notTamion@users.noreply.github.com>
2023-09-30 10:38:45 +0200

    Added IgnoreHeight Command

--- src/main/java/de/tamion/listeners/ChatListener.java
+++ src/main/java/de/tamion/listeners/ChatListener.java
@@ -1,6 +1,8 @@
 package de.tamion.listeners;
 
 import de.tamion.RangeChat;
+import org.bukkit.Location;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -12,13 +14,29 @@ public class ChatListener implements Listener {
     @EventHandler
     public void onChat(PlayerChatEvent e) {
         Player p = e.getPlayer();
-        Double range = RangeChat.getPlugin().getConfig().getDouble("chat.range");
-        if(range != 0.0) {
+        FileConfiguration config = RangeChat.getPlugin().getConfig();
+        double range = config.getDouble("chat.range");
+        boolean ignoreheight = config.getBoolean("chat.ignoreheight");
+        if(range == 0.0) {
+            return;
+        }
+        if(ignoreheight) {
+            new HashSet<>(e.getRecipients()).forEach(target -> {
+                Location ploc = p.getLocation();
+                ploc.setY(0);
+                Location tloc = target.getLocation();
+                tloc.setY(0);
+                if(!p.getWorld().getPlayers().contains(target) || ploc.distance(tloc) >= range) {
+                    e.getRecipients().remove(target);
+                }
+            });
+        } else {
             new HashSet<>(e.getRecipients()).forEach(target -> {
                 if(!p.getWorld().getPlayers().contains(target) || p.getLocation().distance(target.getLocation()) >= range) {
                     e.getRecipients().remove(target);
                 }
             });
         }
+
     }
 }
